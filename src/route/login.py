cb65c1bb66be27a2b57fa95219808f49b293902f
KiharaTakahiro <takahirokihara123@gmail.com>
2020-11-04 21:35:52 +0900

    詳細画面を除くフロントを作成したのでそこまででコミット

--- src/route/login.py
+++ src/route/login.py
@@ -8,7 +8,7 @@ def index():
   return render_template('login.html', title='LOGIN')
 
 # ログイン処理
-@login.route('/auth')
+@login.route('/login')
 def auth():
   # TODO: 認証する処理を記載する
   return redirect(url_for('menu.index'))
